d55375bc15190329e124305f2837a9e394007551
seiya <FCA22010001@edu.fca.ac.jp>
2025-02-24 04:03:58 +0900

    Login画面作成

--- app/src/main/java/com/example/simplenoteapp/ui/auth/LoginScreen.kt
+++ app/src/main/java/com/example/simplenoteapp/ui/auth/LoginScreen.kt
@@ -1,2 +1,83 @@
 package com.example.simplenoteapp.ui.auth
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(
+    navController: NavController,
+    authViewModel: AuthViewModel = AuthViewModel()
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    val isLoggedIn by authViewModel.isLoggedIn.collectAsState()
+
+    // ログイン成功時にホーム画面へ遷移
+    if (isLoggedIn) {
+        navController.navigate("home") {
+            popUpTo("signup") { inclusive = true }
+        }
+    }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "ログイン")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("メールアドレスを入力") }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("パスワード") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            authViewModel.login(email, password)
+        }) {
+            Text(text = "ログイン")
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(onClick = {
+            navController.navigate("signup")
+        }) {
+            Text(text = "新規登録画面へ")
+        }
+    }
+
+}
