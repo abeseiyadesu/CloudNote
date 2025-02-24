1358ad252154124757a549a6727144e97e3577b8
seiya <FCA22010001@edu.fca.ac.jp>
2025-02-24 20:11:31 +0900

    データクラスを作成

--- app/src/main/java/com/example/simplenoteapp/Note.kt
+++ app/src/main/java/com/example/simplenoteapp/Note.kt
@@ -1,3 +1,8 @@
 package com.example.simplenoteapp
 
-data class Note()
+data class Note(
+    val noteId: String = "",
+    val title: String = "",
+    val content: String = "",
+    val timestamp: Long = 0
+)
